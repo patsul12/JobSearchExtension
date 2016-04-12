814a077b8c93277f0620b24c915d4dc29d7a48b1
patsul12 <patsul12@gmail.com>
2016-04-12 11:33:34 -0700

    change request to go to newly create chrome_create method

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
+function getCookies(domain, name, callback) {
+  chrome.cookies.get({"url": domain, "name": name}, function(cookie) {
+    if(callback) {
+      callback(cookie.value);
+    }
+  });
+}
 document.addEventListener('DOMContentLoaded', function() {
+  chrome.cookies.getAll({domain: 'localhost'}, function(cookies) {
+  });
   var checkPageButton = document.getElementById('addLink');
   checkPageButton.addEventListener('click', function() {
-
     chrome.tabs.getSelected(null, function(tab) {
       d = document;
 
       var f = d.createElement('form');
-      f.action = 'http://localhost:3000/job_postings';
+      f.action = 'http://localhost:3000/job_postings/chrome_create';
       f.method = 'post';
       var i = d.createElement('input');
       i.type = 'hidden';
       i.name = 'job_posting[url]';
       i.value = tab.url;
+      var j = d.createElement('input');
+      j.type = 'hidden';
+      j.name = 'job_posting[user_id]';
+      j.value = 1;
       f.appendChild(i);
+      f.appendChild(j);
       d.body.appendChild(f);
       f.submit();
     });
